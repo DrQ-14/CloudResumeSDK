d17249c34fa5eb9c45492b747fb21e47bbb4c038
DrQ-14 <skolwaite@gmail.com>
2026-02-02 17:34:35 -0500

    Configuration Fixed, push after initial docker setup

--- backend/ResumeCounter.Functions/obj/Debug/net8.0/WorkerExtensions/obj/Debug/net6.0/WorkerExtensions.AssemblyInfo.cs
+++ backend/ResumeCounter.Functions/obj/Debug/net8.0/WorkerExtensions/obj/Debug/net6.0/WorkerExtensions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654515e44685fa61e7de1df08694391741214444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353f3de6e661ac38fb9a2538e4c099b66ef8e995")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
