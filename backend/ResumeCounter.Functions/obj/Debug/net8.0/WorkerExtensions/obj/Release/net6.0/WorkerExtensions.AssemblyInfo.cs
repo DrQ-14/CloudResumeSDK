353f3de6e661ac38fb9a2538e4c099b66ef8e995
john <skolwaite@gmail.com>
2026-02-02 17:25:21 -0500

    Began setting up docker and CosmosDB emulator for integration tests.

--- backend/ResumeCounter.Functions/obj/Debug/net8.0/WorkerExtensions/obj/Release/net6.0/WorkerExtensions.AssemblyInfo.cs
+++ backend/ResumeCounter.Functions/obj/Debug/net8.0/WorkerExtensions/obj/Release/net6.0/WorkerExtensions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5cad75fadf2fd2d77a590d83f218cef3ca3e8b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+654515e44685fa61e7de1df08694391741214444")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
