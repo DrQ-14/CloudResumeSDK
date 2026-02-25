4f9ae0e8ea4e046c24b4174abf3d411fb6aa7f5c
DrQ-14 <skolwaite@gmail.com>
2026-02-24 19:12:38 -0500

    Removed local.settings.json from tracked files.

--- backend/ResumeCounter.Functions/Repositories/Cosmos/CounterRepository.cs
+++ backend/ResumeCounter.Functions/Repositories/Cosmos/CounterRepository.cs
@@ -16,11 +16,19 @@ public class CounterRepository : ICounterRepository
 
     public async Task<Counter> GetCounterAsync()
     {
-        var response = await _container.ReadItemAsync<Counter>(
-        id: "resume",
-        partitionKey: new PartitionKey("resume"));
-
-    return response.Resource;
+        try
+        {
+            var response = await _container.ReadItemAsync<Counter>(
+                id: "resume",
+                partitionKey: new PartitionKey("resume"));
+            return response.Resource;
+        }
+        catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            var counter = new Counter { id = "resume", Count = 0 };
+            await _container.CreateItemAsync(counter, new PartitionKey(counter.id));
+            return counter;
+        }
     }
 
     public async Task UpdateCounterAsync(Counter counter)
