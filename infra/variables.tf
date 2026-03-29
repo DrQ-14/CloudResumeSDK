634770cd71cffbd4908e7458751679db3c7ab061
DrQ-14 <skolwaite@gmail.com>
2026-03-29 15:29:08 -0400

    Reverting variable as bootstrap is complete

--- infra/variables.tf
+++ infra/variables.tf
@@ -4,10 +4,10 @@ variable "project_name" {
   type        = string
 }
 
-variable "subscription_id" {
-  description = "Azure Subscription ID for this deployment"
-  type        = string
-}
+#variable "subscription_id" {
+#  description = "Azure Subscription ID for this deployment"
+#  type        = string
+#}
 
 
 variable "environment" {
