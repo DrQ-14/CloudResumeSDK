ee7daabe43f30732662ba7ef3bd0ab55bdb7500a
DrQ-14 <skolwaite@gmail.com>
2026-03-29 21:31:32 -0400

    Fixed yaml duplicate env error

--- infra/main.tf
+++ infra/main.tf
@@ -33,8 +33,11 @@ resource "azurerm_storage_account" "function_storage" {
   name                     = local.storage_name
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
+
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  shared_access_key_enabled = false
 }
 
 #FUNCTION APP CONSUMPTION PLAN
