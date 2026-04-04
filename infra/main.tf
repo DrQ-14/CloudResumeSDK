34088d23e7315c37507fc843d581d338e40774a5
DrQ-14 <skolwaite@gmail.com>
2026-04-03 20:19:53 -0400

    Correcting variable definition in data module.

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ module "core" {
 module "data" {
   source = "./modules/data"
 
-  account_name        = local.cosmos_account_name
+  name                = local.cosmos_account_name
   location            = module.core.location
   resource_group_name = module.core.resource_group_name
 
