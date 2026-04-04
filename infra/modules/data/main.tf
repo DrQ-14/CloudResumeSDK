01cb8c3a7008b7df54f8bb1cf221741e0d8653fa
DrQ-14 <skolwaite@gmail.com>
2026-04-03 20:04:29 -0400

    Redefined account_name attribute in data module.

--- infra/modules/data/main.tf
+++ infra/modules/data/main.tf
@@ -21,14 +21,14 @@ resource "azurerm_cosmosdb_account" "account" {
 resource "azurerm_cosmosdb_sql_database" "database" {
   name                = var.database_name
   resource_group_name = var.resource_group_name
-  name        = azurerm_cosmosdb_account.account.name
+  account_name        = azurerm_cosmosdb_account.account.name
 }
 
 #COSMOS SQL CONTAINER
 resource "azurerm_cosmosdb_sql_container" "container" {
   name                = var.container_name
   resource_group_name = var.resource_group_name
-  name        = azurerm_cosmosdb_account.account.name
+  account_name        = azurerm_cosmosdb_account.account.name
   database_name       = azurerm_cosmosdb_sql_database.database.name
 
   partition_key_paths = [var.partition_key_path]
