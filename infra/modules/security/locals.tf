471b0a0f455644b30b004012005f7ef967538d61
DrQ-14 <skolwaite@gmail.com>
2026-04-06 21:17:05 -0400

    Changed connection string formatting.

--- infra/modules/security/locals.tf
+++ infra/modules/security/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  storage_connection_string = "DefaultEndpointsProtocol=https;AccountName=${trimspace(var.storage_account_name)};AccountKey=${trimspace(var.storage_account_access_key)};EndpointSuffix=core.windows.net"
+  storage_connection_string = "DefaultEndpointsProtocol=https;AccountName=${var.storage_account_name};AccountKey=${var.storage_account_access_key};EndpointSuffix=core.windows.net"
 }
