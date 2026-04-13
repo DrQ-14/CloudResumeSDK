ada7788e97cdba94b38119c23ca4494fbb8a406c
DrQ-14 <skolwaite@gmail.com>
2026-04-13 14:24:16 -0400

    Fixed variable names.

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -10,8 +10,8 @@ resource "azurerm_service_plan" "service_plan" {
 
   resource "azurerm_application_insights" "appi" {
   name                = "resume-dev-appinsights"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   application_type    = "web"
 }
 
