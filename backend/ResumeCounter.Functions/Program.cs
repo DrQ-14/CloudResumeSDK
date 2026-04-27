d852ca9ea1e7aea4ba518c77ff108e56e15f17b9
DrQ-14 <skolwaite@gmail.com>
2026-04-27 01:10:21 -0400

    Editing variables to check for wiring mismatch.

--- backend/ResumeCounter.Functions/Program.cs
+++ backend/ResumeCounter.Functions/Program.cs
@@ -15,42 +15,18 @@ var host = new HostBuilder()
         {
             var endpoint = config["CosmosDb__AccountEndpoint"];
 
-            if (string.IsNullOrEmpty(endpoint))
-                throw new Exception("CosmosDb__AccountEndpoint is NULL");
-
             return new CosmosClient(endpoint, new DefaultAzureCredential());
         });
 
         // Container
         services.AddSingleton(sp =>
         {
-            
             var client = sp.GetRequiredService<CosmosClient>();
 
-            try
-            {
-                var db = client.GetDatabase("CosmosDb__Database");
-                var container = db.GetContainer("CosmosDb__Container");
+            var database= config["CosmosDb__Database"];
+            var container = config["CosmosDb__Container"];
 
-                return container;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Cosmos container resolution failed", ex);
-            }
-            
-            /* var client = sp.GetRequiredService<CosmosClient>();
-
-            var databaseName = config["CosmosDb__Database"];
-            var containerName = config["CosmosDb__Container"];
-
-            if (string.IsNullOrEmpty(databaseName))
-                throw new Exception("CosmosDb__DatabaseName is NULL");
-
-            if (string.IsNullOrEmpty(containerName))
-                throw new Exception("CosmosDb__ContainerName is NULL");
-
-            return client.GetContainer(databaseName, containerName); */
+            return client.GetContainer(database, container);
         });
 
         // Repository
