c9048901616c339563b414d87baddf25551db411
DrQ-14 <skolwaite@gmail.com>
2026-04-27 02:14:23 -0400

    Removed old DI code.

--- backend/ResumeCounter.Functions/Program.cs
+++ backend/ResumeCounter.Functions/Program.cs
@@ -13,9 +13,9 @@ var host = new HostBuilder()
         services.AddSingleton(s =>
         {
             var config = s.GetRequiredService<IConfiguration>();
-        
+
             var endpoint = config["CosmosDb:AccountEndpoint"];
-        
+
             return new CosmosClient(endpoint, new DefaultAzureCredential());
         });
 
@@ -38,41 +38,3 @@ var host = new HostBuilder()
     .Build();
 
 host.Run();
-
-//using Azure.Identity;
-
-/* var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices((context, services) =>
-    {
-        var config = context.Configuration;
-
-        // CosmosClient (Managed Identity)
-        services.AddSingleton(sp =>
-        {
-            var endpoint = config["CosmosDb__AccountEndpoint"];
-
-            return new CosmosClient(endpoint, new DefaultAzureCredential());
-        });
-
-        // Container
-        services.AddSingleton(sp =>
-        {
-            var client = sp.GetRequiredService<CosmosClient>();
-
-            var database= config["CosmosDb__Database"];
-            var container = config["CosmosDb__Container"];
-
-            return client.GetContainer(database, container);
-        });
-
-        // Repository
-        services.AddSingleton<ICounterRepository, CounterRepository>();
-
-        // Service
-        services.AddSingleton<ResumeCounterService>();
-    })
-    .Build();
-
-host.Run();
- */
